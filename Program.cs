17aa54a271eb96a79a9552e11a2827730ac0b820
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 10:31:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarEventsWithLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** More fun with Lambdas\n");
+
+            // Make a car as usual.
+            Car c1 = new Car("Slugbug", 100, 10);
+
+            // Hook into events with lambdas!
+            c1.AboutToBlow += (sender, e) => Console.WriteLine(e.msg);
+            c1.Exploded += (sender, e) => Console.WriteLine(e.msg);
+
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+    }
+}
